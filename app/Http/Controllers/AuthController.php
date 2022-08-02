7180ec53decb662c92ad02b8dc97794100d331c1
Nguyễn Việt Phương <75111251+vnpresent@users.noreply.github.com>
2022-08-02 17:52:27 +0700

    jwt api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -12,8 +14,24 @@ class AuthController extends Controller
         $this->middleware('auth:api', ['except' => ['login']]);
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        $validator = Validator::make($request->all(),[
+            'username'=>'string',
+            'password'=>'string|'
+        ]);
+
+        if ($validator->fails())
+            return response()->json(['error' => 'Unauthorized'], 401);
+
+
+
+        $client = new Client();
+        $response = $client->get('');
+        $value = json_decode($response->getBody(), true);
+
+
+
 //        $credentials = request(['email', 'password']);
 //
 //        if (! $token = auth()->attempt($credentials)) {
@@ -31,7 +49,7 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-
+        auth()->user()->update(['access_token'=>'']);
         return response()->json(['message' => 'Successfully logged out']);
     }
 
