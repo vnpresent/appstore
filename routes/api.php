eb42c7dba8d39a2e952d3e35429683e1da042977
Nguyễn Việt Phương <75111251+vnpresent@users.noreply.github.com>
2022-08-02 17:33:45 +0700

    install jwt

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login',[AuthController::class,'login']);
+    Route::get('/logout',[AuthController::class,'logout']);
+    Route::post('/refresh',[AuthController::class,'refresh']);
+});
